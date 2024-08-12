8dbe51086a095f07fde6d81eddd3a3ab2e6b5c83
HS-hyeokjin <youngurwls@gmail.com>
2024-08-12 14:15:57 +0900

    feat : 결제 서비스
    메세지

--- git-project/src/payment/paymentController.java
+++ git-project/src/payment/paymentController.java
@@ -2,6 +2,7 @@ package payment;
 public class paymentController {
     public static void main(String[] args) {
         System.out.println("결제 컨트롤러");
+        System.out.println("결제 서비스");
     }
 
 }
