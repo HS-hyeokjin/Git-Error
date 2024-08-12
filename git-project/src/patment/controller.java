9cd293fe65d66f2fe2e165941161654d8de50e03
HS-hyeokjin <youngurwls@gmail.com>
2024-08-12 13:11:03 +0900

    feat : zadaksdfjkaf
    askldjaskd

--- git-project/src/patment/controller.java
+++ git-project/src/patment/controller.java
@@ -2,14 +2,6 @@ package patment;
 
 public class controller {
     public static void main(String[] args) {
-        System.out.println("2번의 유틸 ");
-        System.out.println("2번의 유틸 ");
-
-        System.out.println("2번의 유틸 ");
-
-        System.out.println("2번의 유틸 ");
-        System.out.println("2번의 유틸 ");
-
-
+        System.out.println(1);
     }
 }
