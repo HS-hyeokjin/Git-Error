e09e410a7d0813f729e3ba37b4171aaaae0ec74b
HS-hyeokjin <youngurwls@gmail.com>
2024-08-12 14:36:41 +0900

    실수1 커밋

--- git-project/src/util/database.java
+++ git-project/src/util/database.java
@@ -5,5 +5,6 @@ public class database {
         System.out.println("main에서 작업");
         System.out.println("databse 수정 혁진");
         System.out.println("main data 업데이트");
+        System.out.println("mainbranch");
     }
 }
